9fa40187d03d592a771e89130cd246dcd908d479
MiriamPeskowitz <miriam5000@gmail.com>
2019-04-10 22:30:31 -0400

    debugs fetchAllEntries, 2

--- client/src/components/AllEntries.js
+++ client/src/components/AllEntries.js
@@ -7,23 +7,32 @@ import  { fetchAllEntries } from '../actions/entriesActions';
 
 
 class AllEntries extends Component {
-
- const red = {
-    color: '#cd5c5c'
+  componentWillMount() {
+    console.log('in CWM')
+    fetchAllEntries()
   }
- const onClick(e) {
-  e.preventDefault()
-  this.fetchAllEntries()
-  console.log("got to fetchallentries")
- }
 
-  listEntries = () => this.props.entries.map((entry, id, content, title) => <Entry key={id}  title = {title}  content = {content} />)
-    render() {     
+  // this.onClick = this.onClick.bind(this);
+
+  // onClick {
+  //   fetchAllEntries()
+  //   console.log("got to fetchallentries")
+  // }
+  render() {  
+    const { entries } = this.props;
+
+   // < AddEntry onClick=>
+  const listEntries = () => this.props.entries.map((entry, id, content, title) => <Entry key={id}  title = {title}  content = {content} />)
+    
+  
+     const red = {
+      color: '#cd5c5c'
+    }   
       return(
         <div>
-          <h2 style={red} onCLick ={ this.onClick } > Entries</h2>
+          <h2 style={red} > Entries</h2>
           <ul>
-            { this.listEntries }
+            { listEntries }
           </ul>
         </div> 
     )
