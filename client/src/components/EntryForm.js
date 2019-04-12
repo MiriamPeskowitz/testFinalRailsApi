590837b1073f07b4b1a41448a5a2dc95440fb2cb
MiriamPeskowitz <miriam5000@gmail.com>
2019-04-12 17:14:17 -0400

    refactored Home.js, removed class, created function

--- client/src/components/EntryForm.js
+++ client/src/components/EntryForm.js
@@ -1,7 +1,4 @@
-// will be a form to add a new entry 
-// alink that opens the form 
-//  
-import React from 'react';
+ import React from 'react';
 import { Link } from 'react-router-dom';
 import './EntryForm.css';
 //refactor: use sass for all these colors 
@@ -9,32 +6,14 @@ const red = {
     color: '#cd5c5c'
   }
 
-// const align = {
-//     verticalAlign: 'middle'
-//   }
-// const align = {
-//   label textarea{
-//  vertical-align: middle;
-//   }
-// }.formfield {
-//   <label for="textarea">Label for textarea</label>
-//   <textarea id="textarea" rows="5">Textarea</textarea>
-// }
-
 const EntryForm = () =>
- 
- 
-    // <h2 style={red}> New Entry </h2>
-   
-       
-    <form>
+
+  <form>
       <fieldset>
         <legend style={red}> New Entry</legend>
-         
-
+        
         <label htmlFor="Title">Title: <textarea rows="1" cols= "40" name="title"> </textarea></label>
-     
-
+  
         <br/>
 
         <label htmlFor="content">Entry: <textarea rows="5" cols= "50" name="content"></textarea></label>
@@ -43,13 +22,10 @@ const EntryForm = () =>
         <input type="submit" value="Submit" />
 
       </fieldset>
+
     <Link to='/entries' style={red}>Cancel</Link>
+  
   </form>
 
 
 export default EntryForm;
-
-//cancel -- should return to /Entries
-// <textarea rows = "5" cols = "50" name = "description">
-//             Enter your name
-//          </textarea>
--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,15 +1,12 @@
 import React from 'react';
 //what's in this component, other than title? 
-//make an entry? 
-class Home extends React.Component {
 
-  render() {
-    return (
-      <div>
-        <h1 className="App-title">Mourning/Together</h1>
-      </div>
-    )
-  }
+const Home = () => {
+  return (
+    <div>
+      <h1 className="App-title">Mourning/Together</h1>
+    </div>
+  )
 }
 
 export default Home;
